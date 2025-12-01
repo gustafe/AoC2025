c5a95d59458e8b4c3e0680225c7f69ad64155a8c
Gustaf Erikson <gerikson@gmail.com>
2025-12-01 09:46:19 +0100

    added my perl template

---
+++ aoc-template.pl
@@ -0,0 +1,32 @@
+#! /usr/bin/env perl
+use Modern::Perl '2015';
+# useful modules
+use List::Util qw/sum/;
+use Data::Dump qw/dump/;
+use Test::More;
+use Time::HiRes qw/gettimeofday tv_interval/;
+sub sec_to_hms;
+
+my $start_time = [gettimeofday];
+#### INIT - load input data from file into array
+
+my $testing = 0;
+my @input;
+my $file = $testing ? 'test.txt' : 'input.txt';
+open( my $fh, '<', "$file" );
+while (<$fh>) { chomp; s/\r//gm; push @input, $_; }
+
+### CODE
+
+
+### FINALIZE - tests and run time
+# is();
+done_testing();
+say sec_to_hms(tv_interval($start_time));
+
+### SUBS
+sub sec_to_hms {
+    my ($s) = @_;
+    return sprintf("Duration: %02dh%02dm%02ds (%.3f ms)",
+    int( $s/(3600) ), ($s/60) % 60, $s % 60, $s * 1000 );
+}
