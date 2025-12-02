fa30ea0299c958afe3a44de49f39275124b5c364
Gustaf Erikson <gerikson@gmail.com>
2025-12-02 11:24:38 +0100

    perltidy

--- d02-Gift-Shop.pl
+++ d02-Gift-Shop.pl
@@ -23,65 +23,66 @@ while (<$fh>) { chomp; s/\r//gm; push @input, $_; }
 
 ### CODE
 
-my @ranges = split(/,/, $input[0]);
-my $sum;
-my $count;
-my $count_double;
-my %invalids;
+my @ranges = split( /,/, $input[0] );
+my $sum;         # part 1
+my %invalids;    # part 2
 for my $range (@ranges) {
-    my ( $start, $end ) = split(/\-/, $range );
-    for my $code ($start ... $end) {
-	$count++;
-	my %hist;
-	for my $d (split(//, $code)) {
-	    $hist{$d}++
-	}
-	next unless all { $_>1 } values %hist; # no singleton characters
-	my $len = length( $code );
-	# part 1 codes
-	if ($len %2 == 0) {
-	    my $first = substr( $code, 0, $len/2);
-	    my $second= substr( $code, $len/2, $len/2);
-	    if ($second eq $first) {
-		say $code if $testing;
-		$sum += $code;
+    my ( $start, $end ) = split( /\-/, $range );
+    for my $code ( $start ... $end ) {
+        my %hist;
+        for my $d ( split( //, $code ) ) {
+            $hist{$d}++;
+        }
+        next unless all { $_ > 1 } values %hist;    # no singleton characters
+        my $len = length($code);
 
-		$invalids{$code}++
-	    }
-	}
+        # part 1 codes
+        if ( $len % 2 == 0 ) {
+            my $first  = substr( $code, 0,        $len / 2 );
+            my $second = substr( $code, $len / 2, $len / 2 );
+            if ( $second eq $first ) {
+                say $code if $testing;
+                $sum += $code;
 
-	# new rules
-	
-	if (scalar keys %hist == 1) { # one repeated character
-	    say $code if $testing;
-	    $invalids{$code}++;
-	    next;
-	}
-	# split by segments, and compare them
-	for my $segment (2 ... int($len/2)) {
-	    next unless $len % $segment == 0;
-	    my @parts;
-	    for (my $idx=0; $idx<=$len-$segment; $idx += $segment) {
-		push @parts, substr( $code, $idx, $segment );
-	    }
-	    # compare all parts to the first
-	    my $first = $parts[0];
-	    if (all {$_ eq $first } @parts  ) {
-		say $code if $testing;
-		$invalids{$code}++;
-	    }
-	    $invalids{$code}++ if all { $_ eq $first } @parts;
-	      
-	}
+                # also add them for part 2
+                $invalids{$code}++;
+            }
+        }
+
+        # new rules
+
+        if ( scalar keys %hist == 1 ) {    # one repeated character
+            say $code if $testing;
+            $invalids{$code}++;
+            next;
+        }
+
+        # split by segments, and compare these
+        for my $segment ( 2 ... int( $len / 2 ) ) {
+            next unless $len % $segment == 0;
+            my @parts;
+            for ( my $idx = 0; $idx <= $len - $segment; $idx += $segment ) {
+                push @parts, substr( $code, $idx, $segment );
+            }
+
+            # compare all parts to the first
+            my $first = $parts[0];
+            if ( all { $_ eq $first } @parts ) {
+                say $code if $testing;
+                $invalids{$code}++;
+            }
+            $invalids{$code}++ if all { $_ eq $first } @parts;
+
+        }
     }
 }
 my $part2 = sum keys %invalids;
 
 ### FINALIZE - tests and run time
-is($sum, 53420042388, "Part 1: $sum");
-is($part2, 69553832684, "Part 2: $part2");
+is( $sum,   53420042388, "Part 1: $sum" );
+is( $part2, 69553832684, "Part 2: $part2" );
 done_testing();
-say sec_to_hms(tv_interval($start_time));
+say sec_to_hms( tv_interval($start_time) );
 
 ### SUBS
 sub sec_to_hms {
@@ -98,7 +99,8 @@ sub sec_to_hms {
 
 =encoding utf8
 
-It was faster solving this by thinking about than trying to find a regex online to solve it.
+It was faster solving this by thinking about than trying to find a
+regex online to solve it.
 
 Score: 2
 
